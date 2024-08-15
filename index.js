254746bb7c56d9ef37470c5594ddcfed3aaaf529
TanavGP <tanavg03@gmail.com>
2024-08-15 11:44:50 +0530

    debug for dynamic height 2

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-function setDynamicHeight() {
+function setDynamicViewport() {
     let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
+
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+
     const headerElement = document.querySelector('.header');
-    headerElement.innerHTML = 100 * vh;
+    headerElement.innerHTML = String(vw) + ", " + String(vh);
 }
 
-window.addEventListener('resize', setDynamicHeight);
-window.addEventListener('load', setDynamicHeight);
+window.addEventListener('resize', setDynamicViewport);
+window.addEventListener('load', setDynamicViewport);
 
 function testAddBooks(bookArray) {
 
