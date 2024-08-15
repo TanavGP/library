2bf0cc53e8da8d33fc9112f593850e48c4c07360
TanavGP <tanavg03@gmail.com>
2024-08-15 11:11:00 +0530

    Responsiveness for phones using JS

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function setDynamicHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', setDynamicHeight);
+window.addEventListener('load', setDynamicHeight);
+
 function testAddBooks(bookArray) {
-    
+
 }
 
 function Book(name, author, pages, haveRead) {
