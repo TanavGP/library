4a7bd37d3ef9346799d7b1a350256f836c57a7fb
TanavGP <tanavg03@gmail.com>
2024-08-15 10:40:00 +0530

    Added basic structure, pushing to test on mobile devices

---
+++ index.js
@@ -0,0 +1,13 @@
+function testAddBooks(bookArray) {
+    
+}
+
+function Book(name, author, pages, haveRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+}
+
+const bookArray = [];
+
